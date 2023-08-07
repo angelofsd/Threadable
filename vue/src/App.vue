85b573d7112b4490997eae5408caea40c17bf1eb
Stephen Kaczmarowski <srkaz94@gmail.com>
2023-08-07 15:00:05 -0700

    added css to register

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="app">
     <Header></Header>
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div>
+    
     <router-view />
   </div>
 </template>
