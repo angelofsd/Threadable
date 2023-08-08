09f6756a6333bf7b0ef98d84cbeff89ad6541561
Richard Ta <richardlamta@gmail.com>
2023-08-08 14:09:22 -0700

    BE FE Forums update

--- java/database/data.sql
+++ java/database/data.sql
@@ -4,8 +4,8 @@ INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULi
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 INSERT INTO users (username, password_hash, role)
 VALUES ('bozo', 'hashed_password1', 'moderator'), ('user1', 'hashed_password2', 'user'), ('mod2', 'hashed_password3', 'moderator');
-INSERT INTO forums (name, description, created_by)
-VALUES ('Technology', 'Discussion about tech', 1), ('Sports', 'All about sports', 2);
+INSERT INTO forums (name, description, created_by, date_created)
+VALUES ('Technology', 'Discussion about tech', 1, NOW()), ('Sports', 'All about sports', 2, NOW());
 INSERT INTO posts (title, body, forum_id, user_id)
 VALUES ('New Gadgets', 'Latest gadgets in the market...', 1, 2), ('Football Match', 'Last nights game was...', 2, 2);
 INSERT INTO replies (text, post_id, user_id)
--- java/src/main/java/com/techelevator/model/Forum.java
+++ java/src/main/java/com/techelevator/model/Forum.java
@@ -1,4 +1,56 @@
 package com.techelevator.model;
+import java.sql.Timestamp;
 
 public class Forum {
+
+    private int id;
+    private int createdBy;
+    private String name;
+    private String description;
+    private Timestamp dateCreated;
+
+    public Forum() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Timestamp dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+
+
 }
