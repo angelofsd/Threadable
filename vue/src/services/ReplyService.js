fc4dd923838c9a1725ba08fd1d0656935beb83c6
Angel Acosta <angelofsd@gmail.com>
2023-08-08 08:15:01 -0600

    Replyservice complete, started work on ReplyList.vue

--- vue/src/services/ReplyService.js
+++ vue/src/services/ReplyService.js
@@ -1,13 +1,29 @@
 import axios from 'axios';
 
-export default {
+const API_URL = '/posts';
 
-  createReply(postId) {
-    return axios.post(url)
+export default {
+  
+  listByPostId(postId) {
+    return axios.get(`${API_URL}/${postId}/replies`);
   },
 
-  getReplies(postId) {
-    return axios.get('/posts/${postId}/replies');
-  }
+  getReplyById(postId, replyId) {
+    return axios.get(`${API_URL}/${postId}/replies/${replyId}`);
+  },
 
+  createReply(postId, reply) {
+    return axios.post(`${API_URL}/${postId}/replies`, reply);
+  },
+
+  updateReply(postId, replyId, reply) {
+    return axios.put(`${API_URL}/${postId}/replies/${replyId}`, reply);
+  },
+
+  deleteReply(postId, replyId) {
+    return axios.delete(`${API_URL}/${postId}/replies/${replyId}`);
+  }
 }
+
+
+
