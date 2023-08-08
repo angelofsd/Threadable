ba1be2cfc107305acaddf1f4eec4aa986500cccb
Angel Acosta <angelofsd@gmail.com>
2023-08-08 09:28:05 -0600

    added profile_pic_url to users table

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE users (
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
+	profile_pic_url VARCHAR(255),
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
