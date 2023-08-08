35e151bb9483a03193e949e4c4074fda4f825b3c
Angel Acosta <angelofsd@gmail.com>
2023-08-08 16:19:07 -0600

    added 5 forums and 10 posts to database

--- java/database/data.sql
+++ java/database/data.sql
@@ -6,9 +6,13 @@ INSERT INTO users (username, password_hash, role) VALUES ('stephen', '$2a$10$cwl
 INSERT INTO users (username, password_hash, role)
 VALUES ('bozo', 'hashed_password1', 'moderator'), ('user1', 'hashed_password2', 'user'), ('mod2', 'hashed_password3', 'moderator');
 INSERT INTO forums (name, description, created_by, date_created)
-VALUES ('Technology', 'Discussion about tech', 1, NOW()), ('Sports', 'All about sports', 2, NOW());
+VALUES ('Technology', 'Discussion about tech', 1, NOW()), ('Sports', 'All about sports', 2, NOW()), ('Pokemon', 'All about sports', 3, NOW()), ('Programming', 'All Things Coding', 4, NOW()), ('Food', 'All about delicious Food', 5, NOW());
 INSERT INTO posts (title, body, forum_id, user_id)
-VALUES ('New Gadgets', 'Latest gadgets in the market...', 1, 2), ('Football Match', 'Last nights game was...', 2, 2);
+VALUES ('New Gadgets', 'Latest gadgets in the market like omg...', 1, 2), ('Football Match', 'Last nights game was the best ever.', 2, 2), ('I caught a Pikachu', 'After 10 years I caught a Pikachu', 3, 2) ,
+('Golden Cards', 'Has anyone ever heard of catching golden pokemon cards et all i really wish i had some golden cards', 3, 2), ('Bulbasaur is cheating', 'The title says it all, this card should be illegals', 3, 2),
+ ('Java rules', 'Java is way better than Javascript. What an ugly language JavaScript is.', 4, 3), ('JavaScript is amazing', 'I thought JavaScript was horrible but then i used it and like omg omg it was amazing no compiler checks and i can run antyhing', 4, 4),
+('Vue 2 Deprecated', 'Can you believe that Vue 2 is going to be deprecated at the end of the year. What will I do now? I guess i"ll have to learn Vue 3', 4, 4), ('Greek Food', 'I know Greek food and let me tell you, Gustavis is the best Greek food in town, it is not even close omg omg omg.', 5, 5),
+ ('Wheels and Cheese', 'My grandmother passed down a secret recipe of wheels and cheese and it is better than any high end restaurant. First you have to take it out of the box and then boil water. Here is the key, make sure you add a dash of salt to the water. It will be amazing', 5, 5);
 INSERT INTO replies (text, post_id, user_id)
 VALUES ('I love the new gadgets!', 1, 3), ('That was an amazing game!', 2, 1);
 INSERT INTO replies (text, date_created, post_id, user_id)
