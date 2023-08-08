8ac6c36bb934afd98a36a05a5459bd3e02e127d0
Angel Acosta <angelofsd@gmail.com>
2023-08-07 20:15:28 -0600

    Reply model(dto), reply interface dao and jdbcdao, and reply controller. Added a few more entries to the db to test replies. Able to pull list of replies in postman successfully

--- java/src/main/java/com/techelevator/model/Reply.java
+++ java/src/main/java/com/techelevator/model/Reply.java
@@ -1,4 +1,55 @@
 package com.techelevator.model;
+import java.sql.Timestamp;
 
 public class Reply {
+
+    private int id;
+    private String text;
+    private Timestamp dateCreated;
+    private int postId;
+    private int userId;
+
+    public Reply() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Timestamp getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Timestamp dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
