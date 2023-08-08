5ce37d8a395adac337ec38aeda0b67e452ee99d7
Angel Acosta <angelofsd@gmail.com>
2023-08-08 09:35:32 -0700

    added forum view

---
+++ vue/src/views/Forum.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="forum">
+    <h1>Forum Title</h1>
+    <p>This is the forum description</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "forum",
+  components: {},
+  data() {
+    return {
+      forum: {
+        forumName: "",
+        forumDescription: "",
+        forumDateCreated: "",
+        createdBy: 0,
+      },
+      invalidCredentials: false
+    };
+  },
+};
+</script>
