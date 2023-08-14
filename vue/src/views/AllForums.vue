42ca5c8100860d663a78050ffc2aea4610c9bd3e
Stephen Kaczmarowski <srkaz94@gmail.com>
2023-08-14 13:02:46 -0700

    fixed search and added the ability to update local storage to keep better track of faves and likes

--- vue/src/views/AllForums.vue
+++ vue/src/views/AllForums.vue
@@ -10,6 +10,7 @@
 import CreateForum from '../components/CreateForum.vue';
 import ForumList from '../components/ForumList.vue'
 import CreatePost from '../components/CreatePost.vue'
+import ForumService from '../services/ForumService';
 
 export default {
   name: "allForums",
@@ -24,6 +25,24 @@ export default {
       forumName:"",
       invalidCredentials: false
     };
+  },
+  created() {
+    ForumService.getFavorited(this.$store.state.user.id).then((response) => {
+            if (response.status === 200) {
+                this.forums = response.data;
+                this.$store.commit("SET_FAVORITED_FORUMS", response.data)
+            }
+        })
+        .catch( (error) => {
+        if (error.response) {
+            alert("Something went wrong: " + error.response.statusText);
+        } else if(error.request){
+                    //We could not reach the server
+                    alert("We could not reach the server");
+        } else {
+                    alert("Something went horribly wrong");
+                }
+        })
   }
 };
 </script>
